fee1f3140f4d6cad31161677b165c6df1b981a89
kyro-D <kyle.rose@colorado.edu>
2018-12-04 22:01:48 -0700

    added python code

--- NodeJS/dbHandler.py
+++ NodeJS/dbHandler.py
@@ -1,2 +1,5 @@
 #Python code to handle queries and database connections
 #need psycopg library for this code to work
+
+#conn = psycopg2.connect("dbname=kyletestdb user=postgres password=")
+print("Inside python script")
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -33,6 +33,25 @@ var router = express.Router();
 
 //code to allow python calls
 var myPythonScriptPath = 'dbHandler.py'
+// Use python shell
+var PythonShell = require('python-shell');
+var pyshell = new PythonShell(myPythonScriptPath);
+
+pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log(message);
+});
+
+// end the input stream and allow the process to exit
+pyshell.end(function (err) {
+    if (err){
+        throw err;
+    };
+
+    console.log('finished');
+});
+
+
 
 var login = require('./routes/login');
 var success =  require('./routes/success');
