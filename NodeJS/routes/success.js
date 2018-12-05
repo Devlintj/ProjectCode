fbd6db4fc2acacd2a2ac30c8af1ac22d7903d8a4
kyro-D <kyle.rose@colorado.edu>
2018-12-04 21:04:41 -0700

    Updated the code so that typing in routes whitout logging in will redirect you to the login page

--- NodeJS/routes/success.js
+++ NodeJS/routes/success.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (request, response) {
-  var validationString = req.query.valid;
+  var validationString = request.query.valid;
   if(validationString === 'success' ){
    console.log('Made it to success root route');
    response.render('success')
